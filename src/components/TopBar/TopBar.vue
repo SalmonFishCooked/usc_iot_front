ad9a1f50f9f62d13390f5e96d3ea6a50d488a24a
salmonfishy <1564309204@qq.com>
2022-09-06 23:32:04 +0800

    界面初始化

--- src/components/TopBar/TopBar.vue
+++ src/components/TopBar/TopBar.vue
@@ -2,10 +2,10 @@
   <div class="w-full topbar bg-white transition h-16 flex items-center">
     <p class="ml-6 font-bold text-base flex-1">设备传感器</p>
     <div class="mx-6 flex p-2">
-      <img class="w-9 rounded-full" src="http://p1.music.126.net/9t8prqgau-c1WD3c6KabZw==/109951167414323368.jpg?param=180y180" alt="">
-      <div class="flex flex-col justify-center ml-2">
+      <img class="w-10 rounded-full" src="http://p1.music.126.net/9t8prqgau-c1WD3c6KabZw==/109951167414323368.jpg?param=180y180" alt="">
+      <div class="justify-center ml-2">
         <p class="text-xs font-bold">南华大学物联网</p>
-        <p class="text-xs text-gray-600">超级用户</p>
+        <t-tag theme="warning" size="small" @click="onClick">超级用户</t-tag>
       </div>
     </div>
   </div>
